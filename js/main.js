da2238a6aa38c34387bb3fa67a9b3c33a47711e0
Spenser Holstein <spenserholstein@Spensers-MBP.fios-router.home>
2017-07-26 14:26:11 -0400

    created objects and functions

--- js/main.js
+++ js/main.js
@@ -1,25 +1,57 @@
 
-var cards = ['queen', 'queen', 'king', 'king'];
 
+// array of card objects
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+	];
+
+// array of cards in hand
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+// function to check for match
+var checkForMatch = function() {
+		if (cardsInPlay.length === 2) {
+			if (cardsInPlay[0] === cardsInPlay[1]) {
+				alert("You found a match!"); 
+			} else {
+				alert("Sorry, try again.")
+			}
+		}
+};
 
-var cardTwo = cards[2];
+// function to flip a card
 
-cardsInPlay.push(cardOne);
+var flipCard = function(cardId) {
 
-cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cards[cardId].rank + ".");
+	console.log("Suit =  " + cards[cardId].suit + ".");
+	console.log("Image = " + cards[cardId].cardImage + ".");
 
-console.log("User flipped " + cardOne);
+	cardsInPlay.push(cards[cardId])
 
-console.log("User flipped " + cardTwo);
+	checkForMatch();
+};
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!"); 
-	} else {
-		alert("Sorry, try again.")
-	}
-}
 
+flipCard(0);
+
+flipCard(2);
